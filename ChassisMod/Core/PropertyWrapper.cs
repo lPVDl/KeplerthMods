5276e161296834e674faeff2474a867f80824312
lPVDl <pvd1313@gmail.com>
2019-08-13 22:36:45 +0300

    Refactored PropertyWrappers. Action<T> can now be used with PropertyWrappers.

--- ChassisMod/Core/PropertyWrapper.cs
+++ ChassisMod/Core/PropertyWrapper.cs
@@ -1,56 +1,99 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Common.Reflection;
+using System;
 
 namespace ChassisMod.Core
 {
     partial class DataWrapper<TConfig>
     {
-        public sealed class PropertyWrapper<TProperty, TPropertyID> where TPropertyID : PropertyIdentity
+        public abstract class PropertyWrapper<TProperty, TPropertyID> where TPropertyID : PropertyIdentity
         {
-            private DataWrapper<TConfig> Source { get; set; }
-            private Func<TProperty, TProperty> Modification { get; set; }
-            private TProperty Data { get; set; }
+            private sealed class WithSource : PropertyWrapper<TProperty, TPropertyID>
+            {
+                public DataWrapper<TConfig> Source { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set)
+                {
+                    var patchInfo = $"{target}.{propertyName} = {Source}.{propertyName}";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        var table = ReflectionHelper.GetStaticFieldValue<TConfig>("Table") as Dictionary<int, TConfig>;
+                        var value = get(table[Source.ID]);
+                        set(config, value);
+                    });
+                }
+            }
+
+            private sealed class WithModification : PropertyWrapper<TProperty, TPropertyID>
+            {
+                public Func<TProperty, TProperty> Modification { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set)
+                {
+                    var patchInfo = $"{target}.{propertyName} = f(x)";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        var value = get(config);
+                        value = Modification(value);
+                        set(config, value);
+                    });
+                }
+            }
+
+            private sealed class WithAction : PropertyWrapper<TProperty, TPropertyID>
+            {
+                public Action<TProperty> Action { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set)
+                {
+                    var patchInfo = $"{target}.{propertyName} = f(x)";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        var value = get(config);
+                        Action(value);
+                        set(config, value);
+                    });
+                }
+            }
+
+            private sealed class WithData : PropertyWrapper<TProperty, TPropertyID>
+            {
+                public TProperty Data { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set)
+                {
+                    var patchInfo = $"{target}.{propertyName} = {Data}";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        set(config, Data);
+                    });
+                }
+            }
 
             public static implicit operator PropertyWrapper<TProperty, TPropertyID> (TProperty data)
             {
                 if (typeof(TProperty).IsClass && data == null)
                     throw new ArgumentNullException("data was null");
 
-                return new PropertyWrapper<TProperty, TPropertyID>() { Data = data };
+                return new WithData() { Data = data };
             }
 
             public static implicit operator PropertyWrapper<TProperty, TPropertyID>(Func<TProperty, TProperty> modification)
             {
-                return new PropertyWrapper<TProperty, TPropertyID>()
-                {
-                    Modification = modification ?? throw new ArgumentNullException("modification was null")
-                };
+                return new WithModification() { Modification = modification ?? throw new ArgumentNullException("modification was null") };
             }
 
             public static implicit operator PropertyWrapper<TProperty, TPropertyID>(DataWrapper<TConfig> source)
             {
-                return new PropertyWrapper<TProperty, TPropertyID>()
-                {
-                    Source = source ?? throw new ArgumentNullException("source was null")
-                };
+                return new WithSource() { Source = source ?? throw new ArgumentNullException("source was null") };
             }
 
-            private PropertyWrapper() { }
-
-            internal void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set)
+            public static implicit operator PropertyWrapper<TProperty, TPropertyID>(Action<TProperty> action)
             {
-                if (Source != null)
-                {
-                    PropertyPatcher.PatchFromSource(Source, target, propertyName, get, set);
-                }
-                else if (Modification != null)
-                {
-                    PropertyPatcher.PatchFromModification(Modification, target, propertyName, get, set);
-                }
-                else
-                {
-                    PropertyPatcher.PatchFromData(Data, target, propertyName, set);
-                }
+                return new WithAction() { Action = action ?? throw new ArgumentNullException("action was null") };
             }
+
+            internal abstract void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set);
         }  
     }    
 }
--- ChassisMod/Core/SerializedPropertyWrapper.cs
+++ ChassisMod/Core/SerializedPropertyWrapper.cs
@@ -1,21 +1,79 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Common.Reflection;
+using System;
 
 namespace ChassisMod.Core
 {
     partial class DataWrapper<TConfig>
     {
-        public sealed class SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>
+        public abstract class SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID> where TPropertyID : PropertyIdentity
         {
-            private Func<TSerialized, TSerialized> Modification { get; set; }
-            private DataWrapper<TConfig> Source { get; set; }
-            private TSerialized Data { get; set; }
+            private sealed class WithModification : SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>
+            {
+                public Func<TSerialized, TSerialized> Modification { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set, Func<TProperty, TSerialized> serialize, Func<TSerialized, TProperty> deserialize)
+                {
+                    var patchInfo = $"{target}.{propertyName} = f(x)";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        var value = serialize(get(config));
+                        value = Modification(value);
+                        set(config, deserialize(value));
+                    });
+                }
+            }
+
+            private sealed class WithAction : SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>
+            {
+                public Action<TSerialized> Action { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set, Func<TProperty, TSerialized> serialize, Func<TSerialized, TProperty> deserialize)
+                {
+                    var patchInfo = $"{target}.{propertyName} = f(x)";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        var value = serialize(get(config));
+                        Action(value);
+                        set(config, deserialize(value));
+                    });
+                }
+            }
+
+            private sealed class WithSource : SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>
+            {
+                public DataWrapper<TConfig> Source { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set, Func<TProperty, TSerialized> serialize, Func<TSerialized, TProperty> deserialize)
+                {
+                    var patchInfo = $"{target}.{propertyName} = {Source}.{propertyName}";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        var table = ReflectionHelper.GetStaticFieldValue<TConfig>("Table") as Dictionary<int, TConfig>;
+                        var value = get(table[Source.ID]);
+                        set(config, value);
+                    });
+                }
+            }
+
+            private sealed class WithData : SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>
+            {
+                public TSerialized Data { get; set; }
+
+                internal override void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set, Func<TProperty, TSerialized> serialize, Func<TSerialized, TProperty> deserialize)
+                {
+                    var patchInfo = $"{target}.{propertyName} = {Data}";
+                    target.AddModification(patchInfo, config =>
+                    {
+                        var value = deserialize(Data);
+                        set(config, value);
+                    });
+                }
+            }
 
             public static implicit operator SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>(DataWrapper<TConfig> source)
             {
-                return new SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>()
-                {
-                    Source = source ?? throw new ArgumentNullException("source was null")
-                };
+                return new WithSource() { Source = source ?? throw new ArgumentNullException("source was null") };
             }
 
             public static implicit operator SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>(TSerialized data)
@@ -23,35 +81,22 @@ namespace ChassisMod.Core
                 if (typeof(TSerialized).IsClass && data == null)
                     throw new ArgumentNullException("data was null");
 
-                return new SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>() { Data = data };
+                return new WithData() { Data = data };
             }
 
             public static implicit operator SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>(Func<TSerialized, TSerialized> modification)
             {
-                return new SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>()
-                {
-                    Modification = modification ?? throw new ArgumentNullException("modification was null")
-                };
+                return new WithModification() { Modification = modification ?? throw new ArgumentNullException("modification was null") };
             }
 
-            private SerializedPropertyWrapper() { }
-
-            internal void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set,
-                Func<TProperty, TSerialized> serialize, Func<TSerialized, TProperty> deserialize)
+            public static implicit operator SerializedPropertyWrapper<TProperty, TSerialized, TPropertyID>(Action<TSerialized> action)
             {
-                if (Source != null)
-                {
-                    PropertyPatcher.PatchFromSource(Source, target, propertyName, get, set);
-                }
-                else if (Modification != null)
-                {
-                    PropertyPatcher.PatchFromSerializedModification(Modification, target, propertyName, get, set, serialize, deserialize);
-                }
-                else
-                {
-                    PropertyPatcher.PatchFromSerializedData(Data, target, propertyName, set, deserialize);
-                }
+                return new WithAction() { Action = action ?? throw new ArgumentNullException("action was null") };
             }
+
+            internal SerializedPropertyWrapper() { }
+
+            internal abstract void Patch(DataWrapper<TConfig> target, string propertyName, Func<TConfig, TProperty> get, Action<TConfig, TProperty> set, Func<TProperty, TSerialized> serialize, Func<TSerialized, TProperty> deserialize);
         }
     }
 }
