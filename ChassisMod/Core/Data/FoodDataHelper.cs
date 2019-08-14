665461d386a1748661731b0f464a3c5222d2a60b
lPVDl <pvd1313@gmail.com>
2019-08-14 16:32:45 +0300

    Made FoodDataHelper comment generator friendlier.

--- ChassisMod/Core/Data/FoodDataHelper.cs
+++ ChassisMod/Core/Data/FoodDataHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using ChassisMod.Core.Data.Util;
+using System.Linq;
 using DataBase;
 using System;
 using Common;
@@ -34,8 +35,31 @@ namespace ChassisMod.Core.Data
 
                 if (LanguageDataHelper.TryGetInEnglish(food.EatBuffDescription, out var info)) { result.Add(info); }
 
-                var props = FormatUtil.GetPropertiesWithValues(food, nameof(food.EatBuffDescription));
-                result.AddRange(props);
+                if (food.Buff.Length > 0 && food.Buff[0] != 0)
+                {
+                    var effects = food.Buff.Select(x => StatusEffectDataHelper.Instance.NameFor(x));
+                    result.Add($"Effect: [ {string.Join(", ", effects)} ]");
+                }
+
+                if (food.BuffRandomOne.Length > 0 && food.BuffRandomOne[0] != 0 && food.RandomTimes > 0)
+                {
+                    var effects = food.BuffRandomOne.Select(x => StatusEffectDataHelper.Instance.NameFor(x));
+                    result.Add($"RandomEffect: {food.RandomTimes} -> [ {string.Join(", ", effects)} ]");
+                }
+
+                if (food.EffectHp != 0) { result.Add($"PlayerHealth: {food.EffectHp / 100}%"); }
+
+                if (food.EffectHunger != 0) { result.Add($"PlayerSatiety: {food.EffectHunger / 360}%"); }
+
+                if (food.PetEatHp != 0) { result.Add($"MinionHealth: {food.PetEatHp / 100}%"); }
+
+                if (food.PetEatHunger != 0) { result.Add($"MinionSatiety: {food.PetEatHunger / 60}%"); }
+
+                if (food.FeedAnimalHunger != 0) { result.Add($"BreedingSatiety: {food.FeedAnimalHunger}u"); }
+
+                if (food.ExEffect != 0) { result.Add($"SpecialEvent: #{food.ExEffect}"); }
+
+                if (food.UseCD != 0) { result.Add($"Cooldown: {food.UseCD}s"); }
 
                 return result;
 
