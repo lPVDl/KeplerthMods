66540f82c6270ffc1b41d0502743ff1913125f0f
lPVDl <pvd1313@gmail.com>
2019-08-19 12:04:05 +0300

    Made ConfigWrapper.Table initialization more safe.

--- ChassisMod/Wrapping/ConfigWrapper.cs
+++ ChassisMod/Wrapping/ConfigWrapper.cs
@@ -1,11 +1,40 @@
 ﻿using System.Collections.Generic;
-using Common.Reflection;
+using System.Reflection;
 using DataBase;
+using Common;
+using System;
 
 namespace ChassisMod.Wrapping
 {
     internal abstract partial class ConfigWrapper<TConfig> : Entity where TConfig : DBBase
     {
-        public static Dictionary<int, TConfig> Table => Reflector.GetFieldValue(typeof(TConfig), "Table") as Dictionary<int, TConfig>;
+        private static FieldInfo TableRefence { get; }
+
+        public static Dictionary<int, TConfig> Table
+        {
+            get
+            {
+                var table = TableRefence.GetValue(null);
+                if (table == null) throw new Exception($"{typeof(TConfig).Name}.Table was null");
+
+                return table as Dictionary<int, TConfig>;
+            }
+        }
+
+        static ConfigWrapper()
+        {
+            TableRefence = typeof(TConfig).GetField("Table", BindingFlags.Public | BindingFlags.Static);
+
+            if (TableRefence == null)
+            {
+                Log.Error($"{typeof(TConfig).Name}.Table was not found!");
+                return;
+            }
+
+            if (TableRefence.FieldType != typeof(Dictionary<int, TConfig>))
+            {
+                Log.Error($"{typeof(TConfig).Name}.Table has invalid type!");
+            }
+        }
     }
 }
