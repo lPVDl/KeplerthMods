ff8aec8ab91f3cf6cda1eac7a06c86f4e348453d
Victor Fuchadzhy <pvd1313@gmail.com>
2020-02-18 19:26:43 +0200

    Restored SpriteScanner

---
+++ Chassis/Chassis/DataExport.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using Keplerth;
+using System;
+
+namespace Chassis
+{
+    [StaticConstructorOnStartup]
+    public class DataExport
+    {
+        public DataExport()
+        {
+            try
+            {
+                SpriteScanner.ExportAll(@"T:/Keplerth/Sprites/");
+            }
+            catch (Exception e)
+            {
+                Log.Exception(e);
+            }
+        }
+    }
+}
---
+++ Chassis/Chassis/SpriteScanner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using System;
+using Common;
+
+namespace Chassis
+{
+    public static class SpriteScanner
+    {
+        public static void ExportAll(string folder)
+        {
+            if (!Directory.Exists(folder)) { Directory.CreateDirectory(folder); }
+
+            Log.Message("Started exporting sprites");
+
+            ExportSprites(LoadAll<Sprite>("Icon"), Path.Combine(folder, "Icons\\"));
+            ExportSprites(LoadAll<Sprite>("Weapon"), Path.Combine(folder, "Weapons\\"));
+            ExportSprites(LoadAll<Sprite>("Plant"), Path.Combine(folder, "Plants\\"));
+            ExportSprites(LoadAll<Sprite>("Wall"), Path.Combine(folder, "Walls\\"));
+
+            Resources.UnloadUnusedAssets();
+        }
+
+        private static IEnumerable<T> LoadAll<T>(string path) where T : UnityEngine.Object
+        {
+            if (typeof(T) == typeof(Sprite))
+            {
+                return from obj in Resources.LoadAll<GameObject>(path)
+                       let render = obj.GetComponent<SpriteRenderer>()
+                       where render != null && render.sprite != null
+                       select render.sprite as T;
+            }
+            else
+            {
+                throw new NotImplementedException("Unkown type token - " + typeof(T).Name);
+            }
+        }
+
+        private static void ExportSprites(IEnumerable<Sprite> sprites, string folder)
+        {
+            if (!Directory.Exists(folder)) { Directory.CreateDirectory(folder); }
+
+            foreach (var sprite in sprites)
+            {
+                var texture = SpriteUtil.Render(sprite);
+                var data = texture.EncodeToPNG();
+                var path = CreateUniqueFileName(folder, sprite.name, ".png");
+                File.WriteAllBytes(path, data);
+                File.WriteAllLines(path + ".info", sprite.GetInfo());
+            }
+
+        }
+
+        private static string CreateUniqueFileName(string folder, string fileName, string extension)
+        {
+            var path = Path.Combine(folder, fileName + extension);
+            while (File.Exists(path))
+            {
+                fileName = ContinuousEnumeration(fileName);
+                path = Path.Combine(folder, fileName + extension);
+            }
+
+            return path;
+        }
+
+        private static string ContinuousEnumeration(string name)
+        {
+            var underIndex = name.IndexOf('_');
+            if (underIndex < 0) return name + "_0";
+
+            var numberText = name.Substring(underIndex + 1, name.Length - underIndex - 1);
+            if (int.TryParse(numberText, out var value))
+            {
+                value++;
+                return name.Substring(0, underIndex) + "_" + value;
+            }
+
+            return name + "_0";
+        }
+    }
+}
