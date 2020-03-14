da9f833d377ec2bfb959d6d1eb3870bee569cdc4
Victor Fuchadzhy <pvd1313@gmail.com>
2020-03-14 11:33:21 +0200

    InventoryCapacity: Fixed plating pool issue.

---
+++ InventoryCapacity/Scripts/Patchers/Patch_PlatingPool_Awake.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using Harmony;
+using Keplerth;
+using System;
+
+namespace InventoryCapacity.Scripts.Patchers
+{
+    [HarmonyPatch(typeof(PlatingPool), "Awake")]
+    public static class Patch_PlatingPool_Awake
+    {
+        private static void Postfix(PlatingPool __instance)
+        {
+            try
+            {
+                foreach (var obj in PlatingPool.This.MaterialItems)
+                    obj.GetComponent<BagItem>().index += 1000;
+            }
+            catch (Exception e)
+            {
+                Log.ExceptionOnce(e);
+            }
+            
+        }
+    }
+}
